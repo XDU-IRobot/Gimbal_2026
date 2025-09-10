ff6414d49e5999dbb85352bde529fa5c6f607d26
chenmixuexi <chenmixuexi5983@qq.com>
2025-09-10 21:50:28 +0800

    删除build文件。

--- build/Debug/libs/librm/libs/kalman/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/chenm/Desktop/steer_wheel_2025/New/steer_wheel_gimbal/libs/librm/libs/kalman
-# Build directory: C:/Users/chenm/Desktop/steer_wheel_2025/New/steer_wheel_gimbal/build/Debug/libs/librm/libs/kalman
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
